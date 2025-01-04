6813ce40bb80b9fb76304048b822ecd51679bcd7
fisayo-oluwayemi <fisayo@kindredteam.com>
2025-01-04 02:24:53 +0100

    updated Program.cs

--- ML-text-sentiments-analysis/Program.cs
+++ ML-text-sentiments-analysis/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Load sample data
+using ML_text_sentiments_analysis;
+
+var sampleData = new TextsSentimentModel.ModelInput()
+{
+    Col0 = @"Fisayo is super duper not amazing.",
+};
+
+//Load model and predict output
+var result = TextsSentimentModel.Predict(sampleData);
+
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text {sampleData.Col0}\nSentiment : {sentiment}");
